7dae16f36dca873541c3880326f739b42f9dc387
yzang <zangyuan17thu@gmail.com>
2023-09-10 22:01:34 -0400

    a

--- random_labo.py
+++ random_labo.py
@@ -1,8 +1,8 @@
 import numpy as np
 import json
-mutual_info = np.load('mutual_info_labo.npy')
+#mutual_info = np.load('mutual_info_labo.npy')
 
-ids = np.argsort(mutual_info)[::-1]
+ids = np.random.choice(2000,2000,replace=False)
 
 #print(1500,mutual_info[ids[1500]])
 #print(2000,mutual_info[ids[2000]])
@@ -13,14 +13,30 @@ with open('concepts.json', 'r') as fp:
     concepts = json.load(fp)
 class_label=np.load('class_label_labo_new.npy')
 class_label = class_label.T
+
+
 class_ct = {}
 class_concept_id = {}
-
 for c in range(200):
     class_ct[c]=0
     class_concept_id[c]=[]
 new_concepts_id=[]
-ids = np.random.choice(2000,2000,replace=False)
+'''
+for i in range(len(ids)):
+    c_id = ids[i]
+    for c in range(200):
+        if class_label[c_id][c]==1:
+            if class_ct[c]>=5:
+                continue
+            else:
+                class_ct[c] += 1
+                if c_id in new_concepts_id:
+                    continue
+                new_concepts_id.append(c_id)
+                for c in range(200):
+                    if class_label[c_id][c] == 1:
+                        class_concept_id[c].append(c_id)
+'''
 for i in range(len(class_label)):
     c_id = ids[i]
     flag = 0
@@ -28,7 +44,9 @@ for i in range(len(class_label)):
         if class_ct[c] < 2:
             flag = 1
     if flag == 0:
+        print('f',i)
         break
+
 
     for c in range(200):
         if class_label[c_id][c]==1:
@@ -45,8 +63,10 @@ for i in range(len(class_label)):
 
 for i in range(2000):
     c_id = ids[i]
-    if len(new_concepts_id)>=400:
+    if len(new_concepts_id)>=600:
+        print('f', i)
         break
+
     for c in range(200):
         if class_label[c_id][c]==1:
 
@@ -58,18 +78,23 @@ for i in range(2000):
                 if class_label[c_id][c] == 1:
                     class_concept_id[c].append(c_id)
             break
-
+#print(class_ct)
+print(class_concept_id)
+print(len(ids))
 print(len(new_concepts_id))
 print(len(set(new_concepts_id)))
+
+
+
 new_concepts=[]
 new_class_label=class_label[new_concepts_id]
 for i in new_concepts_id:
     new_concepts.append(concepts[i])
 
 new_class_label = new_class_label.T
-with open('concepts_400.json','w') as fw:
+with open('concepts_r_600.json','w') as fw:
     json.dump(new_concepts,fw)
-np.save('class_label_labo_400.npy',new_class_label)
+np.save('class_label_labo_r_600.npy',new_class_label)
 
 
 with open('new_cub_all.json','r') as fp:
@@ -87,9 +112,9 @@ for i in range(len(class_names)):
         cub_fff[n].append(c)
         full_cub_fff[n].append('A photo of a '+ n+', which has '+c)
 
-with open('labo_400.json','w') as fw:
+with open('labo_r_600.json','w') as fw:
     json.dump(cub_fff,fw)
-with open('full_labo_400.json','w') as fw:
+with open('full_labo_r_600.json','w') as fw:
     json.dump(full_cub_fff,fw)
 tl = new_class_label.T
 ct=0
